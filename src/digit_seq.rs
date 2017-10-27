1aeddae705b6f7769bf85dbd0e6824f28258c0cf
Hero Bird <robbepop@web.de>
2017-10-27 14:39:06 +0200

    improved digit_seq module to be more generic over concrete `DigitSeq` and `DigitSeqMut` types.

--- src/digit_seq.rs
+++ src/digit_seq.rs
@@ -2,13 +2,21 @@ use digit::Digit;
 
 use std::slice;
 
+/// A digit sequence is an iterator over `Digit` items.
+pub(crate) trait DigitSeq: Iterator<Item=Digit> {}
+
+/// A mutable digit sequence is an iterator over mutable `Digit` items.
+pub(crate) trait DigitSeqMut<'a>: Iterator<Item=&'a mut Digit> {}
+
 /// A sequence of digits.
+/// 
+/// This is a very efficient `DigitSeq` since its data is contiguous in memory.
 #[derive(Debug, Clone)]
-pub(crate) struct DigitSeq<'a> {
+pub(crate) struct ContiguousDigitSeq<'a> {
 	digits: slice::Iter<'a, Digit>
 }
 
-impl<'a> Iterator for DigitSeq<'a> {
+impl<'a> Iterator for ContiguousDigitSeq<'a> {
 	type Item = Digit;
 
 	#[inline]
@@ -17,13 +25,17 @@ impl<'a> Iterator for DigitSeq<'a> {
 	}
 }
 
+impl<'a> DigitSeq for ContiguousDigitSeq<'a> {}
+
 /// A sequence of mutable digits.
+/// 
+/// This is a very efficient `DigitSeqMut` since its data is contiguous in memory.
 #[derive(Debug)]
-pub(crate) struct DigitSeqMut<'a> {
+pub(crate) struct ContiguousDigitSeqMut<'a> {
 	digits: slice::IterMut<'a, Digit>
 }
 
-impl<'a> Iterator for DigitSeqMut<'a> {
+impl<'a> Iterator for ContiguousDigitSeqMut<'a> {
 	type Item = &'a mut Digit;
 
 	#[inline]
@@ -32,20 +44,26 @@ impl<'a> Iterator for DigitSeqMut<'a> {
 	}
 }
 
+impl<'a> DigitSeqMut<'a> for ContiguousDigitSeqMut<'a> {}
+
 /// A type that acts as a sequence of digits.
 /// 
 /// This is used as abstraction over the different low-level representations
 /// of an `APInt`.
 pub(crate) trait AsDigitSeq {
+	type Seq: DigitSeq;
+
 	/// Returns a sequence of digits.
-	fn digits(&self) -> DigitSeq;
+	fn digits(&self) -> Self::Seq;
 }
 
 /// A type that acts as a sequence of mutable digits.
 /// 
 /// This is used as abstraction over the different low-level representations
 /// of an `APInt`.
-pub(crate) trait AsDigitSeqMut {
+pub(crate) trait AsDigitSeqMut<'a>: AsDigitSeq {
+	type SeqMut: DigitSeqMut<'a>;
+
 	/// Returns a sequence of mutable digits.
-	fn digits_mut(&mut self) -> DigitSeqMut;
+	fn digits_mut(&mut self) -> Self::SeqMut;
 }
