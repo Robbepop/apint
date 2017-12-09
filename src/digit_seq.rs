26a18706f0b3ea938f9e7f99f6fbc0200f2ee5fd
Hero <robbepop@web.de>
2017-12-09 19:53:17 +0100

    Added lifetime specifier to AsDigitSeq trait for better lifetime tracking.

--- src/digit_seq.rs
+++ src/digit_seq.rs
@@ -3,7 +3,7 @@ use digit::Digit;
 use std::slice;
 
 /// A digit sequence is an iterator over `Digit` items.
-pub(crate) trait DigitSeq: Iterator<Item=Digit> {}
+pub(crate) trait DigitSeq<'a>: Iterator<Item=Digit> + 'a {}
 
 /// A mutable digit sequence is an iterator over mutable `Digit` items.
 pub(crate) trait DigitSeqMut<'a>: Iterator<Item=&'a mut Digit> {}
@@ -25,7 +25,7 @@ impl<'a> Iterator for ContiguousDigitSeq<'a> {
 	}
 }
 
-impl<'a> DigitSeq for ContiguousDigitSeq<'a> {}
+impl<'a> DigitSeq<'a> for ContiguousDigitSeq<'a> {}
 
 /// A sequence of mutable digits.
 /// 
@@ -50,20 +50,20 @@ impl<'a> DigitSeqMut<'a> for ContiguousDigitSeqMut<'a> {}
 /// 
 /// This is used as abstraction over the different low-level representations
 /// of an `ApInt`.
-pub(crate) trait AsDigitSeq {
-	type Seq: DigitSeq;
+pub(crate) trait AsDigitSeq<'a> {
+	type Seq: DigitSeq<'a>;
 
 	/// Returns a sequence of digits.
-	fn digits(&self) -> Self::Seq;
+	fn digits(self) -> Self::Seq;
 }
 
 /// A type that acts as a sequence of mutable digits.
 /// 
 /// This is used as abstraction over the different low-level representations
 /// of an `ApInt`.
-pub(crate) trait AsDigitSeqMut<'a>: AsDigitSeq {
+pub(crate) trait AsDigitSeqMut<'a> {
 	type SeqMut: DigitSeqMut<'a>;
 
 	/// Returns a sequence of mutable digits.
-	fn digits_mut(&mut self) -> Self::SeqMut;
+	fn digits_mut(self) -> Self::SeqMut;
 }
