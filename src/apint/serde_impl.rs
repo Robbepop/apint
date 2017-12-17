3f0c98d28868508d5adc5869b35c252a32a760f7
Hero <robbepop@web.de>
2017-12-17 14:48:29 +0100

    Added initial implementation for serde serialize of ApInt.

--- src/apint/serde_impl.rs
+++ src/apint/serde_impl.rs
@@ -1,1 +1,27 @@
-// TODO
+use apint::{ApInt};
+use digit::{Digit};
+
+use serde::{
+    Serialize,
+    Serializer
+};
+
+impl Serialize for Digit {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_u64(self.repr())
+    }
+}
+
+impl Serialize for ApInt {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        use serde::ser::SerializeStruct;
+        let mut s = serializer.serialize_struct("ApInt", 3)?;
+        s.serialize_field("width", &(self.len.to_usize() as u64))?;
+        s.serialize_field("digits", self.as_digit_slice())?;
+        s.end()
+    }
+}
