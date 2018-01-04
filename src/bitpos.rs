5c884e72eaf89e40658cb7da0045f19c1b9bd9ae
Hero <robbepop@web.de>
2018-01-04 23:18:56 +0100

    Restricted visibility of `BitPos::to_pos_within_digit` and `BitPos::to_digit_and_bit_pos` to pub(crate).

--- src/bitpos.rs
+++ src/bitpos.rs
@@ -33,14 +33,14 @@ impl BitPos {
 	/// Converts this `BitPos` into its associated `BitPos` that is usable to operate
 	/// on `Digit` instances.
 	#[inline]
-	pub fn to_pos_within_digit(self) -> BitPos {
+	pub(crate) fn to_pos_within_digit(self) -> BitPos {
 		BitPos(self.0 % digit::BITS)
 	}
 
 	/// Splits this `BitPos` that may range over several `Digit`s within an `ApInt`
 	/// into the associated `Digit` offset and its `Digit`-relative bit position.
 	#[inline]
-	pub fn to_digit_and_bit_pos(self) -> (DigitPos, BitPos) {
+	pub(crate) fn to_digit_and_bit_pos(self) -> (DigitPos, BitPos) {
 		let digit_pos = DigitPos::from(self.0 / digit::BITS);
 		let bit_pos = BitPos::from(self.0 % digit::BITS);
 		(digit_pos, bit_pos)
