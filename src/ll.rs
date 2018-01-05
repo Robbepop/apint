a6fb0fa62dca1715cd45b6fa1d2c2053ffcea76c
Hero <robbepop@web.de>
2018-01-05 22:01:44 +0100

    Renamed ll::carry_add_inout to ll::carry_add and improved doc-comment.

--- src/ll.rs
+++ src/ll.rs
@@ -3,10 +3,16 @@ use digit::{Digit};
 /// Returns the result of a carry-add between `a` and `b` with
 /// the given `carry`.
 /// 
-/// The carry acts as input and will also store the
-/// carry of this operation after the carry-add computation.
+/// # Computes & Returns
+/// 
+/// result = a + b + carry
+/// 
+/// # Note
+/// 
+/// - The carry acts as input and will also store the
+///   carry of this addition after the carry-add computation.
 #[inline]
-pub(crate) fn carry_add_inout(a: Digit, b: Digit, carry: &mut Digit) -> Digit {
+pub(crate) fn carry_add(a: Digit, b: Digit, carry: &mut Digit) -> Digit {
 	let (hi, lo) = (a.dd() + b.dd() + carry.dd()).hi_lo();
 	*carry = hi;
 	lo
