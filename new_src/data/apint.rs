1d8f2031eeeadde92e8b492274f6d6a06182dd6d
Aaron Kutch <aaronkutch@att.net>
2019-07-12 13:14:28 -0500

    Move the rest of mod.rs

--- new_src/data/apint.rs
+++ new_src/data/apint.rs
@@ -3,6 +3,30 @@ use crate::info::{BitWidth, Width, Result, Error};
 
 use std::ptr::NonNull;
 
+/// An arbitrary precision integer with modulo arithmetics similar to machine integers.
+pub struct ApInt {
+    /// The width in bits of this `ApInt`.
+    len : BitWidth,
+    /// The actual data (bits) of this `ApInt`.
+    data: ApIntData
+}
+
+union ApIntData {
+    /// Inline storage (up to 64 bits) for small-space optimization.
+    inl: Digit,
+    /// Extern storage (>64 bits) for larger `ApInt`s.
+    ext: NonNull<Digit>
+}
+
+/// `ApInt` is safe to send between threads since it does not own
+/// aliasing memory and has no reference counting mechanism like `Rc`.
+unsafe impl Send for ApInt {}
+
+/// `ApInt` is safe to share between threads since it does not own
+/// aliasing memory and has no mutable internal state like `Cell` or `RefCell`.
+unsafe impl Sync for ApInt {}
+
+
     /// Deallocates memory that may be allocated by this `ApInt`.
     /// 
     /// `ApInt` instances with a bit width larger than `64` bits
