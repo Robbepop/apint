eb8abcaeb90f6c405e1c890af691fcc1e46ab1e6
Aaron Kutch <aaronkutch@att.net>
2019-07-12 13:45:50 -0500

    fix rebase artifacts

--- src/data.rs
+++ src/data.rs
@@ -9,6 +9,3 @@ mod utils;
 
 pub(crate) use self::{digit::{DigitRepr, Digit, DoubleDigit}, digit_seq::ContiguousDigitSeq, digit_seq::ContiguousDigitSeqMut, storage::Storage, access::{DataAccess, DataAccessMut, ZipDataAccess,ZipDataAccessMutSelf,ZipDataAccessMutBoth}};
 pub use self::{apint::ApInt,uint::UInt, int::Int};
-
-#[cfg(test)]
-pub(crate) use self::apint::ApIntData;
