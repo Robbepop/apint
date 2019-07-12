dfc4eae13ae0e8f6203df29d97e0dfb7e2f111c3
Aaron Kutch <aaronkutch@att.net>
2019-07-12 13:32:28 -0500

    Update utils.rs

--- new_src/data/utils.rs
+++ new_src/data/utils.rs
@@ -1,7 +1,8 @@
 use crate::data::{Digit, ApInt};
 use crate::info::{Result, Width};
 
-
+/// # Utility & Helper Methods
+impl ApInt {
     /// Computes the given operation on all digits of this `ApInt`.
     /// 
     /// # Note
@@ -10,10 +11,10 @@ use crate::info::{Result, Width};
     /// operation for all digits within this `ApInt` as this operation
     /// uses the most efficient way to do so.
     #[inline]    
-    pub(in apint) fn modify_digits<F>(&mut self, f: F)
+    pub(crate) fn modify_digits<F>(&mut self, f: F)
         where F: Fn(&mut Digit)
     {
-        use self::DataAccessMut::*;
+        use crate::data::DataAccessMut::*;
         match self.access_data_mut() {
             Inl(digit) => f(digit),
             Ext(digits) => {
@@ -32,14 +33,14 @@ use crate::info::{Result, Width};
     /// Prefer this utility method for these use cases since this operation
     /// uses the most efficient way to perform the specified task.
     #[inline]    
-    pub(in apint) fn modify_zipped_digits<F>(&mut self, rhs: &ApInt, f: F) -> Result<()>
+    pub(crate) fn modify_zipped_digits<F>(&mut self, rhs: &ApInt, f: F) -> Result<()>
         where F: Fn(&mut Digit, Digit)
     {
-        use self::ZipDataAccessMutSelf::*;
+        use crate::data::ZipDataAccessMutSelf::*;
         match self.zip_access_data_mut_self(rhs)? {
             Inl(lhs, rhs) => f(lhs, rhs),
             Ext(lhs, rhs) => {
-                for (l, &r) in lhs.into_iter().zip(rhs) {
+                for (l, &r) in lhs.iter_mut().zip(rhs) {
                     f(l, r)
                 }
             }
@@ -47,14 +48,13 @@ use crate::info::{Result, Width};
         Ok(())
     }
 
-
-
     /// Returns the most significant `Digit` of this `ApInt`.
     #[inline]    
-    pub(in apint) fn most_significant_digit(&self) -> Digit {
+    pub(crate) fn most_significant_digit(&self) -> Digit {
+        use crate::data::DataAccess::*;
         match self.access_data() {
-            DataAccess::Inl(digit) => digit,
-            DataAccess::Ext(digits) => {
+            Inl(digit) => digit,
+            Ext(digits) => {
                 *digits.last().unwrap()
             }
         }
@@ -62,10 +62,11 @@ use crate::info::{Result, Width};
 
     /// Returns a mutable reference to the most significant `Digit` of this `ApInt`.
     #[inline]    
-    pub(in apint) fn most_significant_digit_mut(&mut self) -> &mut Digit {
+    pub(crate) fn most_significant_digit_mut(&mut self) -> &mut Digit {
+        use crate::data::DataAccessMut::*;
         match self.access_data_mut() {
-            DataAccessMut::Inl(digit) => digit,
-            DataAccessMut::Ext(digits) => {
+            Inl(digit) => digit,
+            Ext(digits) => {
                 digits.last_mut().unwrap()
             }
         }
@@ -73,17 +74,16 @@ use crate::info::{Result, Width};
 
     /// Returns the least significant `Digit` of this `ApInt`.
     #[inline]    
-    pub(in apint) fn least_significant_digit(&self) -> Digit {
+    pub(crate) fn least_significant_digit(&self) -> Digit {
+        use crate::data::DataAccess::*;
         match self.access_data() {
-            DataAccess::Inl(digit) => digit,
-            DataAccess::Ext(digits) => digits[0]
+            Inl(digit) => digit,
+            Ext(digits) => digits[0]
         }
     }
 
-    /// Returns `Bit::Set` if the most significant bit of this `ApInt` is set
-    /// and `Bit::Unset` otherwise.
     #[inline]    
-    pub(in apint) fn most_significant_bit(&self) -> Bit {
+    pub(crate) fn most_significant_bit(&self) -> bool {
         let sign_bit_pos = self.width().sign_bit_pos();
         self.most_significant_digit()
             .get(sign_bit_pos.to_pos_within_digit())
@@ -92,10 +92,8 @@ use crate::info::{Result, Width};
                      operation cannot fail.")
     }
 
-    /// Returns `Bit::Set` if the least significant bit of this `ApInt` is set
-    /// and `Bit::Unset` otherwise.
     #[inline]    
-    pub(in apint) fn least_significant_bit(&self) -> Bit {
+    pub(crate) fn least_significant_bit(&self) -> bool {
         self.least_significant_digit().least_significant_bit()
     }
 
@@ -103,92 +101,25 @@ use crate::info::{Result, Width};
     /// 
     /// # Example
     /// 
-    /// An `ApInt` with a `BitWidth` of `100` bits requires
-    /// 2 `Digit`s for its internal value representation,
-    /// each having 64-bits which totals in `128` bits for the
-    /// `ApInt` instance.
+    /// An `ApInt` with a `BitWidth` of `100` bits requires 2 `Digit`s for its internal value
+    /// representation, each having 64-bits which totals in `128` bits for the `ApInt` instance.
     /// So upon a call to `ApInt::clear_unused_bits` the upper
     /// `128-100 = 28` bits are cleared (set to zero (`0`)).
     #[inline]    
-    pub(in apint) fn clear_unused_bits(&mut self) {
+    pub(crate) fn clear_unused_bits(&mut self) {
         if let Some(bits) = self.width().excess_bits() {
-            self.most_significant_digit_mut()
-                .retain_last_n(bits)
-                .expect("`BitWidth::excess_bits` always returns a number of \
-                         bits that can safely forwarded to `Digit::retain_last_n`.");
+            *self.most_significant_digit_mut() &= !(Digit::ONES << bits);
         }
-    }
-
-    /// Returns `true` if this `ApInt` represents the value zero (`0`).
-    /// 
-    /// # Note
-    /// 
-    /// - Zero (`0`) is also called the additive neutral element.
-    /// - This operation is more efficient than comparing two instances
-    ///   of `ApInt` for the same reason.
-    #[inline]    
-    pub fn is_zero(&self) -> bool {
-        match self.access_data() {
-            DataAccess::Inl(digit) => digit.is_zero(),
-            DataAccess::Ext(digits) => {
-                digits.into_iter().all(|digit| digit.is_zero())
-            }
-        }
-    }
-
-    /// Returns `true` if this `ApInt` represents the value one (`1`).
-    /// 
-    /// # Note
-    /// 
-    /// - One (`1`) is also called the multiplicative neutral element.
-    /// - This operation is more efficient than comparing two instances
-    ///   of `ApInt` for the same reason.
-    #[inline]    
-    pub fn is_one(&self) -> bool {
-        match self.access_data() {
-            DataAccess::Inl(digit) => digit == Digit::one(),
-            DataAccess::Ext(digits) => {
-                let (last, rest) = digits.split_last().unwrap_or_else(|| unreachable!());
-                last.is_one() && rest.into_iter().all(|digit| digit.is_zero())
-            }
-        }
-    }
-
-    /// Returns `true` if this `ApInt` represents an even number.
-    /// Equivalent to testing if the least significant bit is zero.
-    #[inline]
-    pub fn is_even(&self) -> bool {
-        self.least_significant_bit() == Bit::Unset
-    }
-
-    /// Returns `true` if this `ApInt` represents an odd number.
-    /// Equivalent to testing if the least significant bit is one.
-    #[inline]
-    pub fn is_odd(&self) -> bool {
-        self.least_significant_bit() == Bit::Set
-    }
-
-    /// Returns `true` if the **signed** representation of this `ApInt` is positive.
-    /// Equivalent to testing if the most significant bit is zero.
-    #[inline]
-    pub fn is_positive(&self) -> bool {
-        self.most_significant_bit() == Bit::Unset
-    }
-
-    /// Returns `true` if the **signed** representation of this `ApInt` is negative.
-    /// Equivalent to testing if the most significant bit is one.
-    #[inline]
-    pub fn is_negative(&self) -> bool {
-        self.most_significant_bit() == Bit::Set
     }
 
     /// Splits the least significant digits from the rest of the digit slice
     /// and returns it as well as the remaining part of the digit slice.
     #[inline]    
-    pub(in apint) fn split_least_significant_digit(&self) -> (Digit, &[Digit]) {
+    pub(crate) fn split_least_significant_digit(&self) -> (Digit, &[Digit]) {
+        use crate::data::DataAccess::*;
         match self.access_data() {
-            DataAccess::Inl(digit) => (digit, &[]),
-            DataAccess::Ext(digits) => {
+            Inl(digit) => (digit, &[]),
+            Ext(digits) => {
                 let (lsd, rest) = digits.split_first()
                     .expect("An `ApInt` always has at least one digit so calling \
                              `split_first` on a slice of its digits will never \
@@ -201,10 +132,11 @@ use crate::info::{Result, Width};
     /// Splits the most significant digits from the rest of the digit slice
     /// and returns it as well as the remaining part of the digit slice.
     #[inline]    
-    pub(in apint) fn split_most_significant_digit(&self) -> (Digit, &[Digit]) {
+    pub(crate) fn split_most_significant_digit(&self) -> (Digit, &[Digit]) {
+        use crate::data::DataAccess::*;
         match self.access_data() {
-            DataAccess::Inl(digit) => (digit, &[]),
-            DataAccess::Ext(digits) => {
+            Inl(digit) => (digit, &[]),
+            Ext(digits) => {
                 let (lsd, rest) = digits.split_last()
                     .expect("An `ApInt` always has at least one digit so calling \
                              `split_last` on a slice of its digits will never \
@@ -221,25 +153,25 @@ mod tests {
 
     #[test]
     fn most_significant_bit() {
-        assert_eq!(Bit::Unset,
-            ApInt::from_bit(false).most_significant_bit());
-        assert_eq!(Bit::Set,
-            ApInt::from_bit(true).most_significant_bit());
-        assert_eq!(Bit::Unset,
+        assert_eq!(false,
+            ApInt::from_bool(false).most_significant_bit());
+        assert_eq!(true,
+            ApInt::from_bool(true).most_significant_bit());
+        assert_eq!(false,
             ApInt::from_u8(0b0101_0101).most_significant_bit());
-        assert_eq!(Bit::Set,
+        assert_eq!(true,
             ApInt::from_u8(0b1101_0101).most_significant_bit());
-        assert_eq!(Bit::Unset,
+        assert_eq!(false,
             ApInt::from_u16(0b0111_1000_1101_0101).most_significant_bit());
-        assert_eq!(Bit::Set,
+        assert_eq!(true,
             ApInt::from_u16(0b1011_0001_0101_0101).most_significant_bit());
-        assert_eq!(Bit::Unset,
+        assert_eq!(false,
             ApInt::from_u32(0x7000_0000).most_significant_bit());
-        assert_eq!(Bit::Set,
+        assert_eq!(true,
             ApInt::from_u32(0x8000_0000).most_significant_bit());
-        assert_eq!(Bit::Unset,
+        assert_eq!(false,
             ApInt::from_u64(0x70FC_A875_4321_1234).most_significant_bit());
-        assert_eq!(Bit::Set,
+        assert_eq!(true,
             ApInt::from_u64(0x8765_4321_5555_6666).most_significant_bit());
     }
 }
