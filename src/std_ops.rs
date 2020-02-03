7880805da2da4a71a019f5c5d97f437376590cc5
Aaron Kutch <aaronkutch@att.net>
2020-02-03 12:07:21 -0600

    Gather the rest of the `std::ops` from uint.rs and int.rs

--- src/std_ops.rs
+++ src/std_ops.rs
@@ -19,7 +19,9 @@
 
 use crate::{
     ApInt,
+    Int,
     ShiftAmount,
+    UInt,
 };
 
 use std::ops::{
@@ -31,143 +33,216 @@ use std::ops::{
     BitOrAssign,
     BitXor,
     BitXorAssign,
+    Div,
+    DivAssign,
     Mul,
     MulAssign,
     Neg,
     Not,
+    Rem,
+    RemAssign,
     Shl,
     ShlAssign,
+    Shr,
+    ShrAssign,
     Sub,
     SubAssign,
 };
 
-// unary
+// This is done through a macro to decrease the amount of testing needed, since
+// if it works for the `ApInt` case, it will also work for `Int` and `UInt`. It
+// also decreases code duplication.
+macro_rules! common_std_ops {
+    ($ty:ty) => {
+        // unary
 
-impl Not for ApInt {
-    type Output = ApInt;
+        impl Not for $ty {
+            type Output = Self;
 
-    fn not(self) -> Self::Output {
-        self.into_bitnot()
-    }
+            fn not(self) -> Self::Output {
+                self.into_bitnot()
+            }
+        }
+
+        // `std` implements this even for unsigned primitives, and this crate does so
+        // too.
+        impl Neg for $ty {
+            type Output = Self;
+
+            fn neg(self) -> Self::Output {
+                self.into_wrapping_neg()
+            }
+        }
+
+        impl<S> Shl<S> for $ty
+        where
+            S: Into<ShiftAmount>,
+        {
+            type Output = Self;
+
+            fn shl(self, shift_amount: S) -> Self::Output {
+                self.into_wrapping_shl(shift_amount).unwrap()
+            }
+        }
+
+        // binary ops
+
+        impl<'a> BitAnd<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn bitand(self, rhs: &'a Self) -> Self::Output {
+                self.into_bitand(rhs).unwrap()
+            }
+        }
+
+        impl<'a> BitOr<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn bitor(self, rhs: &'a Self) -> Self::Output {
+                self.into_bitor(rhs).unwrap()
+            }
+        }
+
+        impl<'a> BitXor<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn bitxor(self, rhs: &'a Self) -> Self::Output {
+                self.into_bitxor(rhs).unwrap()
+            }
+        }
+
+        impl<'a> Add<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn add(self, rhs: &'a Self) -> Self::Output {
+                self.into_wrapping_add(rhs).unwrap()
+            }
+        }
+
+        impl<'a> Sub<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn sub(self, rhs: &'a Self) -> Self::Output {
+                self.into_wrapping_sub(rhs).unwrap()
+            }
+        }
+
+        impl<'a> Mul<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn mul(self, rhs: &'a Self) -> Self::Output {
+                self.into_wrapping_mul(rhs).unwrap()
+            }
+        }
+
+        // assignment ops
+
+        impl<S> ShlAssign<S> for $ty
+        where
+            S: Into<ShiftAmount>,
+        {
+            fn shl_assign(&mut self, shift_amount: S) {
+                self.wrapping_shl_assign(shift_amount).unwrap();
+            }
+        }
+
+        impl<'a> BitAndAssign<&'a $ty> for $ty {
+            fn bitand_assign(&mut self, rhs: &'a Self) {
+                self.bitand_assign(rhs).unwrap();
+            }
+        }
+
+        impl<'a> BitOrAssign<&'a $ty> for $ty {
+            fn bitor_assign(&mut self, rhs: &'a Self) {
+                self.bitor_assign(rhs).unwrap();
+            }
+        }
+
+        impl<'a> BitXorAssign<&'a $ty> for $ty {
+            fn bitxor_assign(&mut self, rhs: &'a Self) {
+                self.bitxor_assign(rhs).unwrap();
+            }
+        }
+
+        impl<'a> AddAssign<&'a $ty> for $ty {
+            fn add_assign(&mut self, rhs: &'a Self) {
+                self.wrapping_add_assign(rhs).unwrap()
+            }
+        }
+
+        impl<'a> SubAssign<&'a $ty> for $ty {
+            fn sub_assign(&mut self, rhs: &'a Self) {
+                self.wrapping_sub_assign(rhs).unwrap()
+            }
+        }
+
+        impl<'a> MulAssign<&'a $ty> for $ty {
+            fn mul_assign(&mut self, rhs: &'a Self) {
+                self.wrapping_mul_assign(rhs).unwrap();
+            }
+        }
+    };
 }
 
-// `std` implements this even for unsigned primitives, and this crate does so
-// too.
-impl Neg for ApInt {
-    type Output = ApInt;
+common_std_ops!(ApInt);
+common_std_ops!(UInt);
+common_std_ops!(Int);
 
-    fn neg(self) -> Self::Output {
-        self.into_wrapping_neg()
-    }
+// For signedness dependent cases
+macro_rules! signed_std_ops {
+    ($ty:ty) => {
+        impl<S> Shr<S> for $ty
+        where
+            S: Into<ShiftAmount>,
+        {
+            type Output = Self;
+
+            fn shr(self, shift_amount: S) -> Self::Output {
+                self.into_wrapping_shr(shift_amount).unwrap()
+            }
+        }
+
+        impl<'a> Div<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn div(self, rhs: &'a Self) -> Self::Output {
+                self.into_wrapping_div(rhs).unwrap()
+            }
+        }
+
+        impl<'a> Rem<&'a $ty> for $ty {
+            type Output = Self;
+
+            fn rem(self, rhs: &'a Self) -> Self::Output {
+                self.into_wrapping_rem(rhs).unwrap()
+            }
+        }
+
+        impl<S> ShrAssign<S> for $ty
+        where
+            S: Into<ShiftAmount>,
+        {
+            fn shr_assign(&mut self, shift_amount: S) {
+                self.wrapping_shr_assign(shift_amount).unwrap();
+            }
+        }
+
+        impl<'a> DivAssign<&'a $ty> for $ty {
+            fn div_assign(&mut self, rhs: &'a Self) {
+                self.wrapping_div_assign(rhs).unwrap();
+            }
+        }
+
+        impl<'a> RemAssign<&'a $ty> for $ty {
+            fn rem_assign(&mut self, rhs: &'a Self) {
+                self.wrapping_rem_assign(rhs).unwrap();
+            }
+        }
+    };
 }
 
-impl<S> Shl<S> for ApInt
-where
-    S: Into<ShiftAmount>,
-{
-    type Output = ApInt;
-
-    fn shl(self, shift_amount: S) -> Self::Output {
-        self.into_wrapping_shl(shift_amount).unwrap()
-    }
-}
-
-// binary ops
-
-impl<'a> BitAnd<&'a ApInt> for ApInt {
-    type Output = ApInt;
-
-    fn bitand(self, rhs: &'a ApInt) -> Self::Output {
-        self.into_bitand(rhs).unwrap()
-    }
-}
-
-impl<'a> BitOr<&'a ApInt> for ApInt {
-    type Output = ApInt;
-
-    fn bitor(self, rhs: &'a ApInt) -> Self::Output {
-        self.into_bitor(rhs).unwrap()
-    }
-}
-
-impl<'a> BitXor<&'a ApInt> for ApInt {
-    type Output = ApInt;
-
-    fn bitxor(self, rhs: &'a ApInt) -> Self::Output {
-        self.into_bitxor(rhs).unwrap()
-    }
-}
-
-impl<'a> Add<&'a ApInt> for ApInt {
-    type Output = ApInt;
-
-    fn add(self, rhs: &'a ApInt) -> Self::Output {
-        self.into_wrapping_add(rhs).unwrap()
-    }
-}
-
-impl<'a> Sub<&'a ApInt> for ApInt {
-    type Output = ApInt;
-
-    fn sub(self, rhs: &'a ApInt) -> Self::Output {
-        self.into_wrapping_sub(rhs).unwrap()
-    }
-}
-
-impl<'a> Mul<&'a ApInt> for ApInt {
-    type Output = ApInt;
-
-    fn mul(self, rhs: &'a ApInt) -> Self::Output {
-        self.into_wrapping_mul(rhs).unwrap()
-    }
-}
-
-// assignment ops
-
-impl<S> ShlAssign<S> for ApInt
-where
-    S: Into<ShiftAmount>,
-{
-    fn shl_assign(&mut self, shift_amount: S) {
-        self.wrapping_shl_assign(shift_amount).unwrap();
-    }
-}
-
-impl<'a> BitAndAssign<&'a ApInt> for ApInt {
-    fn bitand_assign(&mut self, rhs: &'a ApInt) {
-        self.bitand_assign(rhs).unwrap();
-    }
-}
-
-impl<'a> BitOrAssign<&'a ApInt> for ApInt {
-    fn bitor_assign(&mut self, rhs: &'a ApInt) {
-        self.bitor_assign(rhs).unwrap();
-    }
-}
-
-impl<'a> BitXorAssign<&'a ApInt> for ApInt {
-    fn bitxor_assign(&mut self, rhs: &'a ApInt) {
-        self.bitxor_assign(rhs).unwrap();
-    }
-}
-
-impl<'a> AddAssign<&'a ApInt> for ApInt {
-    fn add_assign(&mut self, rhs: &'a ApInt) {
-        self.wrapping_add_assign(rhs).unwrap()
-    }
-}
-
-impl<'a> SubAssign<&'a ApInt> for ApInt {
-    fn sub_assign(&mut self, rhs: &'a ApInt) {
-        self.wrapping_sub_assign(rhs).unwrap()
-    }
-}
-
-impl<'a> MulAssign<&'a ApInt> for ApInt {
-    fn mul_assign(&mut self, rhs: &'a ApInt) {
-        self.wrapping_mul_assign(rhs).unwrap();
-    }
-}
+signed_std_ops!(UInt);
+signed_std_ops!(Int);
 
 // check that all the operations exist and have the right methods
 #[cfg(test)]
@@ -231,6 +306,42 @@ mod tests {
             x = ApInt::from(3i8);
             x *= &y;
             assert_eq!(x, ApInt::from(21u8));
+        }
+
+        #[test]
+        fn signed() {
+            // Set the sign bit to see if the wrong kind of divisions are being called.
+            // -57i8 has the same bit pattern as 199u8
+            assert_eq!(UInt::from(199u8) >> 4, UInt::from(12u8));
+            assert_eq!(Int::from(-57i8) >> 4, Int::from(-4i8));
+
+            assert_eq!(UInt::from(199u8) / &UInt::from(7u8), UInt::from(28u8));
+            assert_eq!(UInt::from(199u8) % &UInt::from(7u8), UInt::from(3u8));
+            assert_eq!(Int::from(-57i8) / &Int::from(7i8), Int::from(-8i8));
+            assert_eq!(Int::from(-57i8) % &Int::from(7i8), Int::from(-1i8));
+
+            let mut tmp = UInt::from(199u8);
+            tmp >>= 4;
+            assert_eq!(tmp, UInt::from(12u8));
+            let mut tmp = Int::from(-57i8);
+            tmp >>= 4;
+            assert_eq!(tmp, Int::from(-4i8));
+
+            let mut x = UInt::from(199u8);
+            let y = UInt::from(7u8);
+            x /= &y;
+            assert_eq!(x, UInt::from(28u8));
+            x = UInt::from(199u8);
+            x %= &y;
+            assert_eq!(x, UInt::from(3u8));
+
+            let mut x = Int::from(-57i8);
+            let y = Int::from(7i8);
+            x /= &y;
+            assert_eq!(x, Int::from(-8i8));
+            x = Int::from(-57i8);
+            x %= &y;
+            assert_eq!(x, Int::from(-1i8));
         }
     }
 }
